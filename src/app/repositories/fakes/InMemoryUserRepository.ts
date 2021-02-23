966535997b107dadc12d670b74709b0d28782950
Luan Lopes <luanglopes@gmail.com>
2021-02-23 19:38:09 -0300

    feat: implement find unser by email and update user repos on in memory repo

--- src/app/repositories/fakes/InMemoryUserRepository.ts
+++ src/app/repositories/fakes/InMemoryUserRepository.ts
@@ -3,8 +3,17 @@ import {
   CreateDBUserDTO,
   ICreateUserRepository,
 } from 'src/app/repositories/ICreateUSerRepository'
+import {
+  IUpdateUserRepository,
+  UpdateDBUserDTO,
+} from 'src/app/repositories/IUpdateUserRepository'
+import { IFindByUserByEmailRepository } from '../IFindUserByEmailRepository'
 
-export class InMemoryUserRepository implements ICreateUserRepository {
+export class InMemoryUserRepository
+  implements
+    ICreateUserRepository,
+    IUpdateUserRepository,
+    IFindByUserByEmailRepository {
   private users: UserModel[] = []
 
   async create(data: CreateDBUserDTO): Promise<UserModel> {
@@ -16,5 +25,26 @@ export class InMemoryUserRepository implements ICreateUserRepository {
     this.users.push(newUser)
 
     return newUser
+  }
+
+  async update(data: UpdateDBUserDTO): Promise<UserModel> {
+    const userIndex = this.users.findIndex((usr) => usr.id === data.id)
+
+    const user = this.users[userIndex]
+    const updatedUser = {
+      ...user,
+      ...data,
+      password: data.password || user.password,
+    }
+
+    this.users.splice(userIndex, 1, updatedUser)
+
+    return updatedUser
+  }
+
+  async findByEmail(email: string): Promise<UserModel | undefined> {
+    const user = this.users.find((usr) => usr.email === email)
+
+    return user
   }
 }
